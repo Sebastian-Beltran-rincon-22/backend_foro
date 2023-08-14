af8b4c64c8e9fa1baefc24690da418711bd70b76
Brayan <brayantandap@gmail.com>
2023-08-14 16:44:46 -0500

    Update: Add Functionality
    - Added a function that allows updating the publication for 'id' with its router
    - Added a function to delete the selected publication for 'id' with its router

--- controllers/publications.js
+++ controllers/publications.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const Publication = require('../models/publications');
+const publication = require('../models/publications');
 
 const controllerPublication = {
     create: async (req, res) => {
@@ -39,6 +40,38 @@ const controllerPublication = {
             const { id } = req.params
             const publication = await Publication.findById(id)
             res.json(publication)
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+    updatePublication: async (req, res) => {
+        try {
+            const { id } = req.params
+            const user = req.body.user
+            const description = req.body.description
+            const image = req.body.image
+            const reactions = req.body.reactions
+            const comments = req.body.comments
+
+            await Publication.findByIdAndUpdate(id, {
+                user: user,
+                description: description,
+                image: image,
+                reactions: reactions,
+                comments: comments
+            })
+            res.json({ msg: 'Update' })
+        } catch (error) {
+            return res.status(500).json({
+                msg: error.message
+            })
+        }
+    },
+    deletePublication: async (req, res) => {
+        try {
+            const { id } = req.params
+            await publication.findByIdAndDelete(id)
+            res.json({ msg: "Publication deleted" })
         } catch (error) {
             return res.status(500).json({ msg: error.message })
         }
