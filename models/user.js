639a7d25935dfe26d0fefea244574e2f8d48106b
John9135 <palacioalexander5@gmail.com>
2023-08-14 11:17:02 -0500

    feat: created module user, an email validator was implemented, and the bcrypt library was added along with its functions to encrypt the password.

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    user_name: {
+        type: String,
+        required: true,
+        unique: true,
+        maxLenth: 100
+    },
+    user_email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (user_email) {
+                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email);
+            },
+            message: props => `${props.value} is not a valid email`
+        },
+        required: [true, 'user email required']
+    },
+    user_password: {
+        type: String,
+        required: true
+    }
+});
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+
+    if (!user.isModified('user_password')) return next();
+
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.user_password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            user.user_password = hash;
+            next();
+        });
+    });
+});
+
+module.exports = mongoose.model('User', UserSchema)
