b3b1e04903a5bda15474868fab2e168c63ddd287
sewastian <sebastianrincon834@gmail.com>
2023-08-18 12:00:05 -0500

    update [middlewares -> authJwt.js] Import

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const config = require('../config')
 const User = require('../models/user')
-const {Admin} = require('../models/Admin')
+const {Admin} = require('../models/admin')
 
 const authJwt ={
     
